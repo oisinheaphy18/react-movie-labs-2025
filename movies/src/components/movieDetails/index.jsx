711e8e380bb18b20b94e5a6d5b05069f9c1134a0
oisinheaphy18 <20107294@mail.wit.ie>
2025-10-10 21:37:17 +0100

    Exercise: Movie Details Page- included production countries.

--- movies/src/components/movieDetails/index.jsx
+++ movies/src/components/movieDetails/index.jsx
@@ -5,6 +5,8 @@ import Paper from "@mui/material/Paper";
 import Fab from "@mui/material/Fab";
 import NavigationIcon from "@mui/icons-material/Navigation";
 import Drawer from "@mui/material/Drawer";
+import Chip from "@mui/material/Chip";
+import Box from "@mui/material/Box";
 import MovieReviews from "../movieReviews";
 
 const MovieDetails = ({ movie }) => {
@@ -22,7 +24,16 @@ const MovieDetails = ({ movie }) => {
               {movie.overview}
             </Typography>
             <Typography variant="body2">Release: {movie.release_date}</Typography>
-            <Typography variant="body2">Rating: {movie.vote_average}</Typography>
+            <Typography variant="body2" sx={{ mb: 2 }}>
+              Rating: {movie.vote_average}
+            </Typography>
+
+            <Box sx={{ mt: 1 }}>
+              <Chip label="Production Countries" color="primary" sx={{ mr: 1, mb: 1 }} />
+              {(movie.production_countries || []).map((c) => (
+                <Chip key={c.iso_3166_1 + c.name} label={c.name} sx={{ mr: 1, mb: 1 }} />
+              ))}
+            </Box>
           </Paper>
         </Grid>
       </Grid>
